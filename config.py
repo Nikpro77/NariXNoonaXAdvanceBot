7f49a825447baac1c481348976946dfae93ca50c
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-04-15 23:28:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -71,6 +71,9 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b><blockquote>Hᴇʟʟᴏ!! {f
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = True if os.environ.get('CUSTOM_CAPTION', "False") == "True" else False
+
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
